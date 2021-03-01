40d94608ab6fc078f5cea3006134d58ee634789e
CharlesWalkerLudwig <charles.ludwig@mail.utoronto.ca>
2021-02-28 20:30:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ function compute()
     p = document.getElementById("principal").value;
     <button onclick="compute()">Compute Interest</button>
 }
+var principal = "principal"
+var rate = "rate"
+var years = "years" + 2020
+var interest = principal * years * rate / 100
+
         
